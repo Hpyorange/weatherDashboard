973d8babd1db8e18d102b468c9328c7746e68916
Hpyorange <yuemoon45@hotmail.com>
2022-10-17 03:42:36 -0400

    add display main city temp function
    Using js give content
    minor changes of relate css

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,7 +1,4 @@
 var swiper = new Swiper(".mySwiper", {
-    // effect: "coverflow",
-    // grabCursor: true,
-    // centeredSlides: true,
     slidesPerView: 3,
     spaceBetween: 30,
     slidesPerView: "auto",
@@ -14,3 +11,62 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: '.swiper-button-prev',
     },
 });
+
+const currentDay = moment().format('ddd, MMM DD YYYY');
+console.log(currentDay)
+$('#today').text(currentDay);
+
+let searchInput = $('#userInput');
+
+let recent = $('#recentBox');
+
+let lat = "";
+let lon = "";
+
+// display main city 1 temp. 
+let displayMainTemp1 = function(){
+    let cityName = "Toronto"
+    $(`#cityName1`).text(cityName);
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=1d4fe92726b0d3cc08e41e30f8f8511d&units=metric`;
+
+    fetch(url).then(function(response){
+        if(response.ok){
+            response.json().then(function(data) {
+                let icon = data.weather[0].icon;
+                $(`#icon1`).html(`<img src = http://openweathermap.org/img/wn/${icon}.png>`);
+                let temp = Math.round(data.main.temp); 
+                $(`#mainTemp1`).text(`${temp}°C`);
+        });
+    } 
+
+})
+    .catch(function() {
+        alert("Unable to connect, please try again later");
+    });
+};
+
+// display main city 2 temp. 
+let displayMainTemp2 = function(){
+    let cityName = "Vancouver"
+    $(`#cityName2`).text(cityName);
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=1d4fe92726b0d3cc08e41e30f8f8511d&units=metric`;
+
+    fetch(url).then(function(response){
+        if(response.ok){
+            response.json().then(function(data) {
+                let icon = data.weather[0].icon;
+                $(`#icon2`).html(`<img src = http://openweathermap.org/img/wn/${icon}.png>`);
+                let temp = Math.round(data.main.temp); 
+                $(`#mainTemp2`).text(`${temp}°C`);
+        });
+    } 
+
+})
+    .catch(function() {
+        alert("Unable to connect, please try again later");
+    });
+};
+
+//call function
+displayMainTemp1();
+displayMainTemp2();
